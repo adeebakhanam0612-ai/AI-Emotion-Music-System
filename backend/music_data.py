8c5d99e816b298d762bbc9a7dc36d9a8bc86d114
syedasarafatima3828 <syedasarafatima3828@gmail.com>
2026-06-21 16:26:13 +0530

    Update music_data.py

--- backend/music_data.py
+++ backend/music_data.py
@@ -1,84 +1,72 @@
 music_recommendations = {
 
     "happy": [
-        {
-            "name": "Happy - Pharrell Williams",
-            "video": "https://www.youtube.com/watch?v=ZbZSe6N_BXs"
-        },
-        {
-            "name": "Shape of You - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
-        },
-        {
-            "name": "Blinding Lights - The Weeknd",
-            "video": "https://www.youtube.com/watch?v=4NRXx6U8ABQ"
-        }
+        {"name": "Meri Zindagi Hai Tu", "video": "https://www.youtube.com/results?search_query=Meri+Zindagi+Hai+Tu"},
+        {"name": "Arzu", "video": "https://www.youtube.com/results?search_query=Arzu+song"},
+        {"name": "Tum Se", "video": "https://www.youtube.com/results?search_query=Tum+Se+song"},
+        {"name": "Tum Ho Toh", "video": "https://www.youtube.com/results?search_query=Tum+Ho+Toh+song"},
+        {"name": "Ishq", "video": "https://www.youtube.com/results?search_query=Ishq+song"},
+        {"name": "Sadaqay", "video": "https://www.youtube.com/results?search_query=Sadaqay+song"},
+        {"name": "Perfect", "video": "https://www.youtube.com/results?search_query=Perfect+Ed+Sheeran"},
+        {"name": "Soulmate", "video": "https://www.youtube.com/results?search_query=Soulmate+song"},
+        {"name": "Duro Duro", "video": "https://www.youtube.com/results?search_query=Duro+Duro+song"},
+        {"name": "Heeriye", "video": "https://www.youtube.com/results?search_query=Heeriye"}
     ],
 
     "sad": [
-        {
-            "name": "Someone Like You - Adele",
-            "video": "https://www.youtube.com/watch?v=hLQl3WQQoQ0"
-        },
-        {
-            "name": "Fix You - Coldplay",
-            "video": "https://www.youtube.com/watch?v=k4V3Mo61fJM"
-        },
-        {
-            "name": "Let Her Go - Passenger",
-            "video": "https://www.youtube.com/watch?v=RBumgq5yVrA"
-        }
+        {"name": "Nafrat", "video": "https://www.youtube.com/results?search_query=Nafrat+song"},
+        {"name": "Paro", "video": "https://www.youtube.com/results?search_query=Paro+song"},
+        {"name": "Pal Pal", "video": "https://www.youtube.com/results?search_query=Pal+Pal+song"},
+        {"name": "Sahiba", "video": "https://www.youtube.com/results?search_query=Sahiba+song"},
+        {"name": "Guzarish", "video": "https://www.youtube.com/results?search_query=Guzarish+song"},
+        {"name": "Kaash", "video": "https://www.youtube.com/results?search_query=Kaash+song"},
+        {"name": "Ek Lamha", "video": "https://www.youtube.com/results?search_query=Ek+Lamha+song"},
+        {"name": "Mujhe Peene Do", "video": "https://www.youtube.com/results?search_query=Mujhe+Peene+Do"},
+        {"name": "Khasaara", "video": "https://www.youtube.com/results?search_query=Khasaara+song"},
+        {"name": "Main", "video": "https://www.youtube.com/results?search_query=Main+song"}
     ],
 
     "angry": [
-        {
-            "name": "Believer - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
-        },
-        {
-            "name": "Numb - Linkin Park",
-            "video": "https://www.youtube.com/watch?v=kXYiU_JCYtU"
-        },
-        {
-            "name": "Stronger - Kanye West",
-            "video": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"
-        }
+        {"name": "Lakshya", "video": "https://www.youtube.com/results?search_query=Lakshya"},
+        {"name": "Zinda", "video": "https://www.youtube.com/results?search_query=Zinda"},
+        {"name": "Kar Har Maidaan Fateh", "video": "https://www.youtube.com/results?search_query=Kar+Har+Maidaan+Fateh"},
+        {"name": "Brothers Anthem", "video": "https://www.youtube.com/results?search_query=Brothers+Anthem"},
+        {"name": "Sultan Title Track", "video": "https://www.youtube.com/results?search_query=Sultan+Title+Track"},
+        {"name": "Apna Time Aayega", "video": "https://www.youtube.com/results?search_query=Apna+Time+Aayega"},
+        {"name": "Chak De India", "video": "https://www.youtube.com/results?search_query=Chak+De+India"},
+        {"name": "Dangal Title Track", "video": "https://www.youtube.com/results?search_query=Dangal+Title+Track"},
+        {"name": "Soorma Anthem", "video": "https://www.youtube.com/results?search_query=Soorma+Anthem"},
+        {"name": "Toofan", "video": "https://www.youtube.com/results?search_query=Toofan"}
     ],
 
     "neutral": [
-        {
-            "name": "Perfect - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"
-        },
-        {
-            "name": "Photograph - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=nSDgHBxUbVQ"
-        },
-        {
-            "name": "Memories - Maroon 5",
-            "video": "https://www.youtube.com/watch?v=SlPhMPnQ58k"
-        }
+        {"name": "Safarnama", "video": "https://www.youtube.com/results?search_query=Safarnama"},
+        {"name": "Iktara", "video": "https://www.youtube.com/results?search_query=Iktara"},
+        {"name": "Khaabon Ke Parindey", "video": "https://www.youtube.com/results?search_query=Khaabon+Ke+Parindey"},
+        {"name": "Phir Se Ud Chala", "video": "https://www.youtube.com/results?search_query=Phir+Se+Ud+Chala"},
+        {"name": "Yun Hi Chala Chal", "video": "https://www.youtube.com/results?search_query=Yun+Hi+Chala+Chal"},
+        {"name": "Patakha Guddi", "video": "https://www.youtube.com/results?search_query=Patakha+Guddi"},
+        {"name": "Kun Faya Kun", "video": "https://www.youtube.com/results?search_query=Kun+Faya+Kun"},
+        {"name": "Tu Kisi Rail Si", "video": "https://www.youtube.com/results?search_query=Tu+Kisi+Rail+Si"},
+        {"name": "Aao Milo Chalo", "video": "https://www.youtube.com/results?search_query=Aao+Milo+Chalo"},
+        {"name": "Ilahi", "video": "https://www.youtube.com/results?search_query=Ilahi"}
     ],
 
     "surprise": [
-        {
-            "name": "Thunder - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=fKopy74weus"
-        },
-        {
-            "name": "Counting Stars - OneRepublic",
-            "video": "https://www.youtube.com/watch?v=hT_nvWreIhg"
-        }
+        {"name": "Malang", "video": "https://www.youtube.com/results?search_query=Malang"},
+        {"name": "Bang Bang", "video": "https://www.youtube.com/results?search_query=Bang+Bang"},
+        {"name": "Jai Jai Shivshankar", "video": "https://www.youtube.com/results?search_query=Jai+Jai+Shivshankar"},
+        {"name": "Gallan Goodiyan", "video": "https://www.youtube.com/results?search_query=Gallan+Goodiyan"},
+        {"name": "Dil Dhadakne Do", "video": "https://www.youtube.com/results?search_query=Dil+Dhadakne+Do"},
+        {"name": "Badtameez Dil", "video": "https://www.youtube.com/results?search_query=Badtameez+Dil"}
     ],
 
     "fear": [
-        {
-            "name": "Lovely - Billie Eilish",
-            "video": "https://www.youtube.com/watch?v=V1Pl8CzNzCw"
-        },
-        {
-            "name": "Demons - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=mWRsgZuwf_8"
-        }
+        {"name": "Lovely", "video": "https://www.youtube.com/results?search_query=Lovely"},
+        {"name": "Raabta", "video": "https://www.youtube.com/results?search_query=Raabta"},
+        {"name": "Kho Gaye Hum Kahan", "video": "https://www.youtube.com/results?search_query=Kho+Gaye+Hum+Kahan"},
+        {"name": "Agar Tum Saath Ho", "video": "https://www.youtube.com/results?search_query=Agar+Tum+Saath+Ho"},
+        {"name": "Khairiyat", "video": "https://www.youtube.com/results?search_query=Khairiyat"},
+        {"name": "Dil Diyan Gallan", "video": "https://www.youtube.com/results?search_query=Dil+Diyan+Gallan"}
     ]
 }
